e0f09bb7bd524cda0d1a33ac6e0d2254316a8cbc
Coleman Haley <coleman@Colemans-MacBook-Pro.local>
2022-05-07 17:10:33 +0100

    add ssh and fish configs

---
+++ .config/fish/config.fish
@@ -0,0 +1,9 @@
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+eval /Users/coleman/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+# <<< conda initialize <<<
+
+set --global hydro_symbol_prompt λ
+alias vi nvim
+
